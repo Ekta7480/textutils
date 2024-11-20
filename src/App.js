f9de28892929e261ac06863b0a6837d5700cdab6
Ekta7480 <114460852+Ekta7480@users.noreply.github.com>
2024-11-20 21:53:10 +0530

    adding dark mode in about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 
+
 import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
-// import About from './components/About';
+import About from './components/About';
 import Textform from './components/Textform';
 import React, { useState } from 'react';
 // import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
-
-
 
 function App() {
-
   const [mode , setMode]= useState('light'); // Wheather dark mode is enable or not
-  const [alert , setAlert] = useState(null); 
-
+  const [alert , setAlert] = useState(null);
   const showAlert = (message , type)=>{
     setAlert({
       msg:message,
@@ -30,15 +21,13 @@ function App() {
     }, 1500)
   }
  
-
-  
-
+ 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042742';
       showAlert("Dark mode has been enabled", "success");
-      
+     
     }
     else{
       setMode('light');
@@ -46,35 +35,21 @@ function App() {
       showAlert("Light mode has been enabled", "success");
      
     }
-      
+     
   }
   return (
-  
+ 
     <>
-    {/* { <Router> */}
+    
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert = {alert}/>
     <div className="container my- 3">
-    {/* <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-          <Route path="/"> */}
-            <Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-            {/* </Route>
-          </Switch>
-    */}
-        
-          
-    </div>
-    {/* </Router> } */}
-
-
     
+    <Textform showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra Spaces" mode={mode} />
+    <About mode={mode} />
+    </div>
   </>
  
 );
 }
-
 export default App;
-
